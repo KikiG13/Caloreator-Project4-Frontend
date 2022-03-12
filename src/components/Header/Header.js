8988632c435e7b95bbc884211067255650bb38d4
Kian G <kian.gormley13@aol.com>
2022-03-12 13:07:26 -0500

    added styled components & logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,6 +3,7 @@ import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
 import Container from 'react-bootstrap/Container'
 import { Link, NavLink } from 'react-router-dom'
+import '../../index.scss'
 
 const authenticatedOptions = (
   <>
--- src/components/routes/Home.jsx
+++ src/components/routes/Home.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import styled from 'styled-components'
+// import Logo from '../styled/Logo'
 
-const Home = () => (
-  <h4>Caloreator</h4>
-)
+const Logo = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 70vh;
+  // border: 2px solid black;
+`
+
+const Home = () => {
+  return (
+      <Logo>
+        <img src= "https://i.imgur.com/zNsoNvI.png" alt="Caloreator Logo" width="200" height="125"></img>
+      </Logo>
+    )
+}
+
 
 export default Home
